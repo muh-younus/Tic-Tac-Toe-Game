0de5ed51575c4edc735b6f1e8e140ebd819cf4cc
muh-younus <braq369@gmail.com>
2024-08-14 14:40:28 +0500

    add

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-box = document.querySelectorAll(".box");
-btn = document.querySelectorAll("#btn");
+let box1 = document.querySelectorAll(".box");
+let btn = document.querySelectorAll("#btn");
+
+
+
+box1.forEach((boxes) => {
+
+    boxes.addEventListener("click", () => {
+
+        console.log("button click")
+    
+        boxes.innerText= "hello";
+
+    });
+    
+});
